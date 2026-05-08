2bac3dca5904965bf6f3d2bcfab03266bb41f8a0
MrGadget84 <superhyper52@gmail.com>
2026-05-08 17:27:21 +0400

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -12,12 +12,12 @@ SUKI_DIR="drivers/kernelsu"
 KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
-patchesdir="$outside/ksu/sukisu/hooks$(echo $kernel_ver | cut -d. -f1,2)"
-suspatchesdir="$outside/ksu/sukisu/sus/$(echo $kernel_ver | cut -d. -f1,2)"
+patchesdir="$outside/ksu/sukisu/hooks"
+suspatchesdir="$outside/ksu/sukisu/sus/"
 
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
-    git am "$patch_file"
+    patch -p1 < "$patch_file"
   done
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
@@ -26,7 +26,7 @@ fi
 
 if [[ -d "$suspatchesdir" ]]; then
   for patch_file in "$suspatchesdir"/*.patch ; do
-    git am "$patch_file"
+    patch -p1 < "$patch_file"
   done
 else
   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
