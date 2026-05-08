dbb679c0d4bc8228678bca0537b7c3aab96cd426
MrGadget84 <superhyper52@gmail.com>
2026-05-08 17:12:27 +0400

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -13,7 +13,7 @@ KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
 patchesdir="$outside/ksu/sukisu/$(echo $kernel_ver | cut -d. -f1,2)"
-suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
+suspatchesdir="$outside/ksu/sukisu/sus/$(echo $kernel_ver | cut -d. -f1,2)"
 
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
