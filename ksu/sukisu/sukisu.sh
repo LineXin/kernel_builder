72ab1cb584b831da444214581b1d3ef1ca3f30ac
MrGadget84 <superhyper52@gmail.com>
2026-05-11 15:28:40 +0400

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -41,5 +41,3 @@ fi
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-sukisu${KSU_ver}\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
-
-echo -e " \nincludes SukiSU Ultra, ver ${KSU_ver}" >> banner_append
