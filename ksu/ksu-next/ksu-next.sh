eaf53512b4eaa7387ca3cfac0b3026a15943d247
MrGadget84 <superhyper52@gmail.com>
2026-05-12 01:25:07 +0400

    Update ksu-next.sh

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -18,20 +18,18 @@ echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
 echo '# CONFIG_KSU_SUSFS_TRY_UMOUNT is not set' >> "${defconfig_file}"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
-    git am "$patch_file"
+    patch -p1 < "$patch_file"
   done
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
-  exit 1
 fi
 
 if [[ -d "$suspatchesdir" ]]; then
   for patch_file in "$suspatchesdir"/*.patch ; do
-    git am "$patch_file"
+    patch -p1 < "$patch_file"
   done
 else
   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
-  exit 1
 fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ksn${KSU_ver}\"/" "${defconfig_file}"
