4e2435af990b9d9b680a0f662f1c7cec52e97dbb
MrGadget84 <superhyper52@gmail.com>
2026-05-11 19:53:32 +0400

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -16,9 +16,6 @@ KSU_ver=$(($KSU_git_ver + 10000 + 200))
 patchesdir="$outside/ksu/sukisu/hooks"
 suspatchesdir="$outside/ksu/sukisu/sus/"
 
-sed -i 's/susfs_is_sdcard_android_data_decrypted/susfs_is_sdcard_android_data_not_decrypted/g' fs/namespace.c
-sed -i 's/READ_ONCE(susfs_is_sdcard_android_data_not_decrypted)/static_branch_unlikely(\&susfs_is_sdcard_android_data_not_decrypted)/g' fs/namespace.c
-
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     patch -p1 < "$patch_file"
@@ -36,6 +33,9 @@ else
   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
 fi
+
+sed -i 's/susfs_is_sdcard_android_data_decrypted/susfs_is_sdcard_android_data_not_decrypted/g' fs/namespace.c
+sed -i 's/READ_ONCE(susfs_is_sdcard_android_data_not_decrypted)/static_branch_unlikely(\&susfs_is_sdcard_android_data_not_decrypted)/g' fs/namespace.c
 
 if ! grep -q "susfs.h" drivers/kernelsu/supercall/supercall.c 2>/dev/null; then
     sed -i '1i#include <linux/susfs.h>' drivers/kernelsu/supercall/supercall.c
