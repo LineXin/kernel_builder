608759a5355a4b9a6669461ee8c7d3296e026787
MrGadget84 <superhyper52@gmail.com>
2026-05-12 00:17:34 +0400

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -6,8 +6,8 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-# curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-{{builtin}}
-curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-{{builtin}}
+# curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
 git add . && git commit -am "drivers: SukiSU Ultra"
 SUKI_DIR="drivers/kernelsu"
 KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
@@ -32,15 +32,6 @@ if [[ -d "$suspatchesdir" ]]; then
 else
   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
-fi
-
-sed -i '1i#include <linux/susfs_def.h>' fs/notify/fdinfo.c
-
-sed -i 's/susfs_is_sdcard_android_data_decrypted/susfs_is_sdcard_android_data_not_decrypted/g' fs/namespace.c
-sed -i 's/READ_ONCE(susfs_is_sdcard_android_data_not_decrypted)/static_branch_unlikely(\&susfs_is_sdcard_android_data_not_decrypted)/g' fs/namespace.c
-
-if ! grep -q "susfs.h" drivers/kernelsu/supercall/supercall.c 2>/dev/null; then
-    sed -i '1i#include <linux/susfs.h>' drivers/kernelsu/supercall/supercall.c
 fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-sukisu${KSU_ver}\"/" "${defconfig_file}"
