7a5718e77cba0019405a1347b6a515bca588a137
MrGadget84 <superhyper52@gmail.com>
2026-05-12 17:45:45 +0400

    sus

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -12,7 +12,7 @@ KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
 patchesdir="$outside/ksu/ksu-next/patches/$(echo $kernel_ver | cut -d. -f1,2)"
-# suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
+suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
 
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
@@ -23,22 +23,20 @@ else
   exit 1
 fi
 
-# if [[ -d "$suspatchesdir" ]]; then
-#   for patch_file in "$suspatchesdir"/*.patch ; do
-#     git am "$patch_file"
-#   done
-# else
-#   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
-#   exit 1
-# fi
+if [[ -d "$suspatchesdir" ]]; then
+  for patch_file in "$suspatchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
 
 if [[ -f "$patchesdir/fix.sh" ]]; then
   bash "$patchesdir/fix.sh"
 fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ksn${KSU_ver}\"/" "${defconfig_file}"
-
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
-
 echo -e " \nKernelSU-Next Version Enable, ksn ver ${KSU_ver}" >> banner_append
-# echo -e " \nSuSFS Version Enable" >> banner_append
+echo -e " \nSuSFS Version Enable" >> banner_append
