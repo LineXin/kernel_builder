7a6575db5d2d50c96deadb926c723fcafd8d2833
MrGadget84 <superhyper52@gmail.com>
2026-05-12 14:03:26 +0400

    Update ksu-next.sh

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -6,7 +6,8 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy-susfs
+# curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy-susfs
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
@@ -24,13 +25,13 @@ else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
 fi
 
-if [[ -d "$suspatchesdir" ]]; then
-  for patch_file in "$suspatchesdir"/*.patch ; do
-    patch -p1 < "$patch_file"
-  done
-else
-  echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
-fi
+# if [[ -d "$suspatchesdir" ]]; then
+#   for patch_file in "$suspatchesdir"/*.patch ; do
+#     patch -p1 < "$patch_file"
+#   done
+# else
+#   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
+# fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ksn${KSU_ver}\"/" "${defconfig_file}"
 
