e0d133a5f13701938d6093d60c65baf7f2d94c32
MrGadget84 <superhyper52@gmail.com>
2026-05-12 14:48:32 +0400

    Update ksu-next.sh

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -6,33 +6,35 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-# curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy-susfs
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy-susfs
 git add . && git commit -am "drivers: KernelSU"
-KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
-KSU_ver=$(($KSU_git_ver + 30000))
+KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
+KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
-patchesdir="$outside/ksu/ksu-next/patches/"
-suspatchesdir="$outside/ksu/ksu-next/sus_patches/"
+patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
 
-echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
-echo '# CONFIG_KSU_SUSFS_TRY_UMOUNT is not set' >> "${defconfig_file}"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     git am "$patch_file"
   done
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
 fi
 
-# if [[ -d "$suspatchesdir" ]]; then
-#   for patch_file in "$suspatchesdir"/*.patch ; do
-#     git am "$patch_file"
-#   done
-# else
-#   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
-# fi
+if [[ -d "$suspatchesdir" ]]; then
+  for patch_file in "$suspatchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
 
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ksn${KSU_ver}\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}sus\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+
+echo -e " \nKernelSU-Next Version Enable, ksn ver ${KSU_ver}" >> banner_append
+echo -e " \nSuSFS Version Enable" >> banner_append
