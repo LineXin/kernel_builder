6d55b91faf4b09d7e9f212b6d41f160a5f52a804
MrGadget84 <superhyper52@gmail.com>
2026-05-12 18:05:37 +0400

    Update ksu-next.sh

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -32,10 +32,32 @@ else
   exit 1
 fi
 
-if [[ -f "$patchesdir/fix.sh" ]]; then
-  bash "$patchesdir/fix.sh"
+if ! grep -q "int path_umount" fs/namespace.c; then
+    cat <<EOF >> fs/namespace.c
+
+int path_umount(struct path *path, int flags)
+{
+    struct mount *mnt = real_mount(path->mnt);
+    int ret;
+
+    if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+        return -EINVAL;
+    if (!may_mount())
+        return -EPERM;
+    if (path->dentry != path->mnt->mnt_root)
+        return -EINVAL;
+
+    ret = do_umount(mnt, flags);
+
+    dput(path->dentry);
+    mntput_no_expire(mnt);
+    return ret;
+}
+EXPORT_SYMBOL(path_umount);
+EOF
 fi
 
+sed -i '/int do_umount(/a int path_umount(struct path *path, int flags);' include/linux/fs.h
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ksn${KSU_ver}\"/" "${defconfig_file}"
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 echo -e " \nKernelSU-Next Version Enable, ksn ver ${KSU_ver}" >> banner_append
