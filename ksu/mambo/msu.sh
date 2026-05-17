85e453c577284e64fce433e3e48bee5577ce952d
MrGadget84 <superhyper52@gmail.com>
2026-05-17 15:19:18 +0400

    mambosu enable

---
+++ ksu/mambo/msu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# xoposho
+
+export maindir="$(pwd)"
+export outside="${maindir}/.."
+source "${outside}/$1env"
+
+curl -LSs "https://raw.githubusercontent.com/RapliVx/KernelSU/refs/heads/master/kernel/setup.sh" | bash -s master
+git add . && git commit -am "drivers: KernelSU"
+KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
+KSU_ver=$(($KSU_git_ver + 10000 + 200))
+
+patchesdir="$outside/ksu/rksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
+
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"
+
+echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+
+echo -e " \nKernelSU Version Enable, ksu ver ${KSU_ver}" >> banner_append
