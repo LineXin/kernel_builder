ada0711a89eb96d5a65bda7ecba8a6ded7fcc9f2
MrGadget84 <superhyper52@gmail.com>
2026-05-25 15:15:50 +0400

    Create add_path_umount.sh

---
+++ ksu/ksu-next/patches/4.19/add_path_umount.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if ! grep -q "int path_umount" fs/namespace.c; then
+    cat <<EOF >> fs/namespace.c
+
+int path_umount(struct path *path, int flags)
+{
+    struct mount *mnt = real_mount(path->mnt);
+    int ret;
+
+    if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+        return -EINVAL;
+    if (!may_mount())
+        return -EPERM;
+    if (path->dentry != path->mnt->mnt_root)
+        return -EINVAL;
+
+    ret = do_umount(mnt, flags);
+
+    dput(path->dentry);
+    mntput_no_expire(mnt);
+    return ret;
+}
+EXPORT_SYMBOL(path_umount);
+EOF
+fi
+
+if ! grep -q "int path_umount" include/linux/fs.h; then
+    sed -i '/int do_umount(/a int path_umount(struct path *path, int flags);' include/linux/fs.h
+fi
