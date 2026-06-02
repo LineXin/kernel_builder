c2b2c5544e26f71491723cc4486f121b7134c1f4
MrGadget84 <superhyper52@gmail.com>
2026-06-02 14:37:45 +0400

    Update ksu-next.sh

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# white
+# oicnwnwinvf UwU lrfkensfnsdfn
 
 export maindir="$(pwd)"
 export outside="${maindir}/.."
@@ -9,45 +9,34 @@ source "${outside}/$1env"
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
-KSU_ver=$(($KSU_git_ver + 10000 + 200))
+KSU_ver=$(($KSU_git_ver + 30000))
 
-patchesdir="$outside/ksu/ksu-next/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+patchesdir="$outside/ksu/patches/"
+# suspatchesdir="$outside/ksu/sus_patches/"
 
+echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
+echo '# CONFIG_KSU_SUSFS_TRY_UMOUNT is not set' >> "${defconfig_file}"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
-    git apply --ignore-whitespace "$patch_file" || patch -p1 < "$patch_file"
+    git am "$patch_file"
   done
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
 fi
 
-if ! grep -q "int path_umount" fs/namespace.c; then
-    cat <<EOF >> fs/namespace.c
+#if [[ -d "$suspatchesdir" ]]; then
+#  for patch_file in "$suspatchesdir"/*.patch ; do
+#    git am "$patch_file"
+#  done
+#else
+#  echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
+#  exit 1
+#fi
 
-int path_umount(struct path *path, int flags)
-{
-    struct mount *mnt = real_mount(path->mnt);
-    int ret;
-
-    if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-        return -EINVAL;
-    if (!may_mount())
-        return -EPERM;
-    if (path->dentry != path->mnt->mnt_root)
-        return -EINVAL;
-
-    ret = do_umount(mnt, flags);
-
-    dput(path->dentry);
-    mntput_no_expire(mnt);
-    return ret;
-}
-EXPORT_SYMBOL(path_umount);
-EOF
-fi
-
-sed -i '/int do_umount(/a int path_umount(struct path *path, int flags);' include/linux/fs.h
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ksn${KSU_ver}\"/" "${defconfig_file}"
+
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
-echo -e " \nKernelSU-Next Version Enable, ksn ver ${KSU_ver}" >> banner_append
+
+echo -e " \nincludes KernelSU-Next, ver ${KSU_ver}" >> banner_append
+#echo -e " \nincludes SuSFS v2.1.0" >> banner_append
