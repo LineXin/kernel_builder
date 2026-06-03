4a64c2f9efa9fff807cb135c0b340fef918a3d05
MrGadget84 <superhyper52@gmail.com>
2026-06-03 07:54:08 +0400

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -9,10 +9,8 @@ source "${outside}/$1env"
 # curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
 curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
 git add . && git commit -am "drivers: KernelSU"
-if ! grep -q "EXPORT_SYMBOL.*sel_handle_status_ops" security/selinux/selinuxfs.c; then
-  sed -i '/sel_handle_status_ops/,/^};/ s/^};/};\nEXPORT_SYMBOL(sel_handle_status_ops);/' security/selinux/selinuxfs.c
-  echo "EXPORT_SYMBOL added"
-fi
+cp security/selinux/selinuxfs.c drivers/kernelsu/security/selinux/selinuxfs.c 2>/dev/null || true
+cp security/selinux/selinuxfs.c KernelSU/security/selinux/selinuxfs.c 2>/dev/null || true
 SUKI_DIR="drivers/kernelsu"
 KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
